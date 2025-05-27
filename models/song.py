d1b249ceef33fc21161ab8c959f0649b644c820c
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-05-27 04:41:14 -0700

    defining classes

---
+++ models/song.py
@@ -0,0 +1,6 @@
+class Song():
+    def __init__(self, name, minutes_played, artist_name):
+        self.name = name
+        self.minutes_played = minutes_played
+        self.artist_name = artist_name
+
